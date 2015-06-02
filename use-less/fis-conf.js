13a5cc62d7cf66b1e7de4dbc35b99a25ec55e626
xiangshouding <fansekey@gmail.com>
2015-06-02 10:30:08 +0800

    use less

---
+++ use-less/fis-conf.js
@@ -0,0 +1,8 @@
+// npm install -g fis-parser-less-2.x
+fis.config.set('modules.parser.less', 'less-2.x');
+fis.config.set('roadmap.ext.less', 'css');
+fis.config.set('settings.parser.less-2.x', {
+    //假设要给less解析器配置点属性
+});
+
+// fis release -od output # -o 压缩
