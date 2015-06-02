2460da65bf630b222829f9cc1929acad7c2cd50b
xiangshouding <fansekey@gmail.com>
2015-06-02 10:43:20 +0800

    use sass

---
+++ use-sass/fis-conf.js
@@ -0,0 +1,11 @@
+// paser - plugin type
+// scss  - file extension
+// sass  - plugin name
+// npm install -g fis-parser-sass
+fis.config.set('modules.parser.scss', 'sass');
+// from .scss to .css
+fis.config.set('roadmap.ext.scss', 'css');
+// set some option
+fis.config.set('settings.parser.sass', {
+    // fis-parser-sass option
+})
